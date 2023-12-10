e849821bbc7637ab44d5dc700dd31a76c6c9e5d6
HugoLeoNunes <hljnunes@gmail.com>
2023-12-10 01:24:56 -0300

    Aplicando alterações

--- meu_app_front/scripts.js
+++ meu_app_front/scripts.js
@@ -1,6 +1,6 @@
 /*
   --------------------------------------------------------------------------------------
-  Função para modificar a tela inicial.
+  Funções para modificar a tela inicial quando um dos botões é acionado
   --------------------------------------------------------------------------------------
 */
 
@@ -26,29 +26,23 @@ function exibirListar() {
   aparecerLista();
 }
 
-function sumirLista() {
-  var SumirListaDaTelaInicial = document.getElementById('listaDeClientes');
-  SumirListaDaTelaInicial.style = "display:none;";
-}
+/*
+----------------------------------------------------------------------------------------
+  Chamada da função para carregamento da lista de clientes
+----------------------------------------------------------------------------------------
+*/
 
 function aparecerLista () {
   var SumirListaDaTelaInicial = document.getElementById('listaDeClientes');
   SumirListaDaTelaInicial.style = "display:block;";
 }
 
-
-/*
-----------------------------------------------------------------------------------------
-  Chamada da função para carregamento inicial dos dados
-----------------------------------------------------------------------------------------
-*/
-
-
 /*
   --------------------------------------------------------------------------------------
   Função para obter a lista existente do servidor via requisição GET
   --------------------------------------------------------------------------------------
 */
+
 const getList = async () => {
   let url = 'http://127.0.0.1:5000/clientes';
   fetch(url, {
@@ -63,20 +57,12 @@ const getList = async () => {
     });
 }
 
-
-/*
-  --------------------------------------------------------------------------------------
-  Chamada da função para carregamento inicial dos dados
-  --------------------------------------------------------------------------------------
-*/
-//getList();
-
-
 /*
   --------------------------------------------------------------------------------------
   Função para colocar um cliente na lista do servidor via requisição POST
   --------------------------------------------------------------------------------------
 */
+
 const postCliente = async (inputCPF, inputNome, inputTelefone) => {
   const formData = new FormData();
   formData.append('cpf', inputCPF);
@@ -88,12 +74,19 @@ const postCliente = async (inputCPF, inputNome, inputTelefone) => {
     method: 'post',
     body: formData
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.status == 200) {
+        insertList(inputCPF, inputNome, inputTelefone)
+        alert("Cliente adicionado!")
+      } else {
+        alert ("Já existe um cliente cadastrado com este CPF")
+      }
+    }
+    )
     .catch((error) => {
       console.error('Error:', error);
     });
 }
-
 
 /*
   --------------------------------------------------------------------------------------
@@ -109,24 +102,30 @@ const insertButton = (parent) => {
   parent.appendChild(span);
 }
 
-
 /*
   --------------------------------------------------------------------------------------
   Função para remover um cliente da lista de acordo com o click no botão close
   --------------------------------------------------------------------------------------
 */
+
 const removeElement = () => {
   let close = document.getElementsByClassName("close");
-  // var table = document.getElementById('myTable');
   let i;
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
       let div = this.parentElement.parentElement;
       const cpfCliente = div.getElementsByTagName('td')[0].innerHTML
+
+      // removendo a máscara do cpf para deletar o cliente do banco de dados.
+      let cpfRemoveString = cpfCliente.toString()
+      let cpfFormat = cpfRemoveString.replace (/\.|-/gm,'')
+      let cpfClean = parseFloat(cpfFormat)
+
+      // confirmação da remoção.
       if (confirm("Você tem certeza?")) {
         div.remove()
-        deleteCliente(cpfCliente)
-        alert("Removido!")
+        deleteCliente(cpfClean)
+        alert("Cliente foi pro espaço!")
       }
     }
   }
@@ -137,8 +136,8 @@ const removeElement = () => {
   Função para deletar um cliente da lista do servidor via requisição DELETE
   --------------------------------------------------------------------------------------
 */
+
 const deleteCliente = (client) => {
-  console.log(client)
   let url = 'http://127.0.0.1:5000/cliente?cpf=' + client;
   fetch(url, {
     method: 'delete'
@@ -154,6 +153,7 @@ const deleteCliente = (client) => {
   Função para adicionar um novo cliente com cpf, nome e telefone 
   --------------------------------------------------------------------------------------
 */
+
 const newCliente = () => {
   let inputCPF = document.getElementById("newCPF").value;
   let inputNome = document.getElementById("newNome").value;
@@ -164,9 +164,7 @@ const newCliente = () => {
   } else if (isNaN(inputCPF) || isNaN(inputTelefone)) {
     alert("CPF e Telefone precisam ser números!");
   } else {
-    insertList(inputCPF, inputNome, inputTelefone)
     postCliente(inputCPF, inputNome, inputTelefone)
-    alert("Cliente adicionado!")
   }
 }
 
@@ -178,14 +176,27 @@ const newCliente = () => {
 
 const insertList = (cpf, nome, telefone) => {
   var client = [cpf, nome, telefone]
+  let cpfDoBanco = client[0];
+  let telefoneString = client[2].toString()
   var table = document.getElementById('myTable');
   var row = table.insertRow();
+  var clean = parseFloat(client[0])
+
 
   for (var i = 0; i < client.length; i++) {
     var cel = row.insertCell(i);
+    // Aplicando máscara no CPF e telefone do cliente
+    let cpfString = cpfDoBanco.toString()
+    var cpfFormat = cpfString.replace( /(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4")
+    var telefoneFormat = telefoneString.replace( /(\d{2})(\d{4,5})(\d{4})/g, "($1) $2 - $3")
+    client[0] = `${cpfFormat}`
+    client[2] = `${telefoneFormat}`
     cel.textContent = client[i];
   }
+  // retornando o cpf para float.
+  client[0] = clean
   insertButton(row.insertCell(-1))
+  // Sumindo com os dados adicionados nos campos iniciais.
   document.getElementById("newCPF").value = "";
   document.getElementById("newNome").value = "";
   document.getElementById("newTelefone").value = "";
